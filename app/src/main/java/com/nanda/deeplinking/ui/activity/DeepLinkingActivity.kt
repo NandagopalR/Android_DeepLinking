b7641c6db33cbdd6b317cfa643c5baf6d760db5c
Nandagopal R <11364@thehindu.co.in>
2019-05-10 16:57:28 +0530

    Deeplinking implemented

--- app/src/main/java/com/nanda/deeplinking/ui/activity/DeepLinkingActivity.kt
+++ app/src/main/java/com/nanda/deeplinking/ui/activity/DeepLinkingActivity.kt
@@ -1,12 +1,24 @@
 package com.nanda.deeplinking.ui.activity
 
 import android.os.Bundle
+import com.nanda.deeplinking.R
 import com.nanda.deeplinking.ui.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_deeplinking.*
 
 class DeepLinkingActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deeplinking)
+        setSupportActionBar(toolbar)
+        setToolbarTitle("Deep Linking")
+
+        val data = intent.data
+        val action = intent.action
+
+        if (data != null) {
+            tv_deeplink_data.text = data.toString()
+        }
     }
 
 }
